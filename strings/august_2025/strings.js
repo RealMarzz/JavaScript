44aaabb859358a39ae5e02c1ea389da68cbd0ccd
RealMarzz <suisi1972@gmail.com>
2025-08-10 13:58:57 +0100

    Practicing August 2025

--- strings/august_2025/strings.js
+++ strings/august_2025/strings.js
@@ -22,3 +22,55 @@
 // console.log(information)
 // this is concating. Concating is when you add variables together.
 
+// var firstName = "Saabir";
+// var firstName = 'Saabir';
+// var firstName = `Saabir`
+// var firstName = new String("chair");
+// console.log(firstName.length) // length tells me how many characters in a variable
+// console.log(firstName.charAt(3)) //looks for character at an index 
+// console.log(firstName.indexOf("Z")) //looks for an index at a character
+// var firstName = "Saabir Hussein Saabir";
+// console.log(firstName.lastIndexOf("Saabir"))
+
+// var info1 = "Saabir is ";
+// var info2 = "from Somalia and lives ";
+// var info3 = "in the UK.";
+// var information = info1.concat(info2, info3) //concat brings variables together into one variable
+// console.log(information)
+
+// var firstNameL = "saabir"
+// var firstNameU = "SAABIR"
+// console.log(firstNameL.toUpperCase())
+// console.log(firstNameU.toLowerCase())
+
+// var fullName = "Saabir Hussein";
+// console.log(fullName.replace("Hussein", "Mohammed"))
+
+// var fullName = "Saabir Hussein";
+// var firstName = "Saabir"
+// var firstName = /Hussein/g
+// console.log(fullName.startsWith("Saab"));
+// console.log(fullName.endsWith("ein"));
+// console.log(fullName.includes(firstName));
+// console.log(fullName.match(firstName))
+
+// var firstName = "Saabir"
+// console.log(firstName.repeat(9))
+
+// var fullName = "Saabir Hussein Hassan";
+// console.log(firstName.substr(7,7))
+// console.log(fullName.substring(7,fullName.length));
+
+// var firstName = new String("Saabir")
+// console.log(firstName)
+// console.log(firstName.toString())
+// console.log(firstName.valueOf())
+
+// var firstName = "Saabir"
+// console.log(firstName.trim())
+
+// var fullName = "Saabir Hussein"
+// console.log(fullName.slice(3,11))
+
+var fruits = "Apples, Bananas, Oranges, Mangoes, Pears"
+console.log(fruits.split(","))
