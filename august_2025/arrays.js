f711715f9073b890170e6bbf5ad0c33f189c3d84
RealMarzz <suisi1972@gmail.com>
2025-08-24 13:05:08 +0100

    sunday 24th of august 2025

---
+++ august_2025/arrays.js
@@ -0,0 +1,31 @@
+// var names = ["Maryam", "Bob"];
+// console.log(names)
+// var shoppingList = ["Eggs", "Milk", "Bread"];
+// var random = ["hello", 12, true];
+// var names = new Array("Maryam", "Bob", "John", "Alice", "Eve");
+// names[0] = "Saabir";
+// names[1] = "Sara";
+// names.push("Maryam")
+// names.pop()
+// names.length = 2;
+// names.shift();
+// names.unshift("Sara");
+// names.splice(1, 3, "Sara", "Mohammed", "Ahmed");
+// var newNames = names.slice(1, 3);
+// console.log(names);
+// console.log(newNames)
+
+// var mathStudents = ["Bob", "Maryam", "John"];
+// var englishStudents = ["Sara", "Mohammed", "Ahmed"];
+// var students = mathStudents.concat(englishStudents);
+// console.log(students)
+
+// var names = "Zac, Maryam, Bob, John, Alice, Eve";
+// names.sort();
+// names.reverse();
+// var namesArray = names.split(",");
+// console.log(namesArray)
+
+var names = ["Zac", "Maryam", "Bob", "John", "Alice", "Eve"];
+var newNames = names.join(" ")
+console.log(newNames)
---
+++ august_2025/loops.js
@@ -0,0 +1,9 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log("The value of i is " + i);
+// is i less than 10 if true = run code and i++. if false = stops running.
+// }
+var students = ["Tim", "Tom", "John", "Jane", "Bob"];
+// console.log(students.length)
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+} 
